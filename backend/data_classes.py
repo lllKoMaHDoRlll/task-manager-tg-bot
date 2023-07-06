51459ee1552d4e7ad2c855fef4b1eb87b548c034
Koma <aleksandr.orlichenya@gmail.com>
2023-07-06 12:52:44 +0300

    create PriorityLevel

--- backend/data_classes.py
+++ backend/data_classes.py
@@ -1,6 +1,14 @@
 from dataclasses import dataclass
+from enum import Enum
 
 
 @dataclass
 class ConfigData:
     token: str
+
+
+class PriorityLevel(Enum):
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+    NO = 4
