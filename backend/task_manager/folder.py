6d8c6f6dc22ade352d5b13ebb9ed35f5889dcf29
Koma <aleksandr.orlichenya@gmail.com>
2023-07-06 15:31:53 +0300

    create Folder.save, .add_task, remove_task

--- backend/task_manager/folder.py
+++ backend/task_manager/folder.py
@@ -1,6 +1,9 @@
 import json
 from pathlib import Path
 
+from backend.data_classes import PriorityLevel
+from backend.task_manager.task_card import TaskCard
+
 
 class Folder:
     def __init__(self, user_id: int):
@@ -9,3 +12,15 @@ class Folder:
 
     def load(self, path: Path):
         raise NotImplemented
+
+    def save(self, path: Path):
+        raise NotImplemented
+
+    def add_task(self, name: str, parent, description: str = "", due_date=None, repeat=None,
+                 priority: PriorityLevel = PriorityLevel.NO) -> TaskCard:
+        task = TaskCard(name, parent, description, due_date, repeat, priority)
+        self.active_tasks.append(task)
+        return task
+
+    def remove_task(self, task: TaskCard) -> None:
+        self.active_tasks.remove(task)
