717457955c4b4aab0e75f45e9cb35822cb648e5c
Koma <aleksandr.orlichenya@gmail.com>
2023-07-07 16:15:24 +0300

    implement /start command

---
+++ main.py
@@ -0,0 +1,31 @@
+from aiogram import Dispatcher, Bot
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.client.telegram import TelegramAPIServer
+from aiogram.client.session.aiohttp import AiohttpSession
+
+from pathlib import Path
+
+from backend.task_manager.tm_handler import TaskManagerHandler
+from backend._utils import get_config
+
+TASKS_DATA_PATH = Path(r"C:\Users\aleks\PycharmProjects\TG-TaskManager\data\tasks")
+CONFIG_PATH = Path(r"C:\Users\aleks\PycharmProjects\TG-TaskManager\data\config.json")
+
+TM_HANDLER = TaskManagerHandler(TASKS_DATA_PATH)
+
+DP = Dispatcher()
+
+CONFIG = get_config(CONFIG_PATH)
+
+
+@DP.message(Command(commands=['start']))
+async def start_command(message: Message) -> None:
+    await message.answer("Hello! This is your Task Manager Bot.\nYou can list your task with /tasks")
+
+
+if __name__ == '__main__':
+
+    bot = Bot(token=CONFIG.token)
+
+    DP.run_polling(bot)
