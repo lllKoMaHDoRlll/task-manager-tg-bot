0e3df264b8f6752a9cdbfe6ed309734064e35512
Koma <aleksandr.orlichenya@gmail.com>
2023-07-07 13:57:37 +0300

    create TaskManagerHandler

---
+++ backend/task_manager/tm_handler.py
@@ -0,0 +1,13 @@
+
+
+class TaskManagerHandler:
+    def __init__(self):
+        self.folders = dict()
+
+    def load(self):
+        raise NotImplemented
+
+    def save(self):
+        raise NotImplemented
+
+
