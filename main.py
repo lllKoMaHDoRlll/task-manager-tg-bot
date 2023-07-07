fd6e8c17d37ce5dfc6f9fa44a88a371a6dd38d05
Koma <aleksandr.orlichenya@gmail.com>
2023-07-07 19:08:55 +0300

    refactor main.py

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 from aiogram import Dispatcher, Bot
 from aiogram.filters import Command
 from aiogram.types import Message
-from aiogram.client.telegram import TelegramAPIServer
-from aiogram.client.session.aiohttp import AiohttpSession
 
 from pathlib import Path
 
 from backend.task_manager.tm_handler import TaskManagerHandler
 from backend._utils import get_config
 
-TASKS_DATA_PATH = Path(r"C:\Users\aleks\PycharmProjects\TG-TaskManager\data\tasks")
-CONFIG_PATH = Path(r"C:\Users\aleks\PycharmProjects\TG-TaskManager\data\config.json")
 
-TM_HANDLER = TaskManagerHandler(TASKS_DATA_PATH)
+class MyBot:
+    def __init__(self):
+        self.tasks_data_path = Path(r"C:\Users\aleks\PycharmProjects\TG-TaskManager\data\tasks")
+        self.config_path = Path(r"C:\Users\aleks\PycharmProjects\TG-TaskManager\data\config.json")
 
-DP = Dispatcher()
+        self.task_manger_handler = TaskManagerHandler(self.tasks_data_path)
 
-CONFIG = get_config(CONFIG_PATH)
+        self.dispatcher = Dispatcher()
+
+        self.config_data = get_config(self.config_path)
+        self.bot = Bot(token=self.config_data.token)
+
+    def run(self):
+        self.dispatcher.run_polling(self.bot)
+
+    def register_commands(self):
+        self.dispatcher.message.register(start_command, Command(commands=['start']))
+        self.dispatcher.message.register(ping_command, Command(commands=['ping']))
 
 
-@DP.message(Command(commands=['start']))
 async def start_command(message: Message) -> None:
     await message.answer("Hello! This is your Task Manager Bot.\nYou can list your task with /tasks")
 
 
-@DP.message(Command(commands=['ping']))
 async def ping_command(message: Message) -> None:
     await message.answer("pong")
 
 
 if __name__ == '__main__':
-
-    bot = Bot(token=CONFIG.token)
-
-    DP.run_polling(bot)
+    bot = MyBot()
+    bot.register_commands()
+    bot.run()
