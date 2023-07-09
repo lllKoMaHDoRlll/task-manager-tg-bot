1197c5ab4d357d114990f52ed288348396a9a016
Koma <aleksandr.orlichenya@gmail.com>
2023-07-09 14:09:29 +0300

    fix add_task

--- backend/task_manager/folder.py
+++ backend/task_manager/folder.py
@@ -35,9 +35,9 @@ class Folder:
                 return index
         return len(tasks_ids)
 
-    def add_task(self, id: int, name: str, parent=None, description: str = "", due_date=None, repeat=None,
-                 priority: int = 4) -> TaskCard:
-        task = TaskCard(self.get_available_task_id(), name, self, description, due_date, repeat, PriorityLevel(priority))
+    def add_task(self, name: str, parent=None, description: str = "", due_date=None, repeat=None,
+                 priority: int = 4, id: int=None) -> TaskCard:
+        task = TaskCard(self.get_available_task_id(), name, self, description, due_date, repeat, PriorityLevel(int(priority)))
         self.active_tasks.update({task.id: task})
         return task
 
