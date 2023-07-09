5f87112509f02c881f6cdda30d7ff15f70dbf5b4
Koma <aleksandr.orlichenya@gmail.com>
2023-07-09 18:19:21 +0300

    create task manager labels

---
+++ backend/task_manager/labels.py
@@ -0,0 +1,46 @@
+
+EXIT_DIALOG = "You have left the dialog. Bye!✋"
+
+
+SHOW_FOLDERS_TITLE = "🗄 Your folders:\n\n"
+SHOW_FOLDERS_FOLDER_FRAME = "📁 №{folder_id}: {tasks_amount} task(s)\n"
+SHOW_FOLDERS_NO_FOLDERS = "😱 You still have no folders! Let's fix that."
+
+SHOW_FOLDERS_ADD_FOLDER_BUTTON = "🆕 Create new folder."
+SHOW_FOLDERS_SELECT_FOLDER_BUTTON = "📁 № {folder_id}"
+
+EXIT_BUTTON = "❌ Exit"
+BACK_BUTTON = "↩️ Back"
+NOT_IMPLEMENTED_ALERT = "🚧 Work in progress."
+
+CREATE_FOLDER_ALERT = "✅ New folder was created."
+
+SHOW_FOLDER_TITLE = "📁 №{folder_id}:\n\n"
+SHOW_FOLDER_TASK_FRAME = "📕 {name} ❗️: {priority} \n⏰: {due_date} 🔁: {repeat}\n📎: {description}\n\n"
+
+SHOW_FOLDER_NO_TASKS = "🤔 There's nothing! Try add some new tasks!"
+
+ADD_TASK_BUTTON = "⭐️ Add task"
+EDIT_FOLDER_BUTTON = "✏️ Edit folder"
+DELETE_FOLDER_BUTTON = "🗑 Delete folder"
+
+TASK_THUMBNAIL = "📕 {name}"
+
+REQUEST_TASK_NAME = "✍️ Enter task's name.🔴"
+REQUEST_TASK_DESCRIPTION = "✍️ Enter task's description.🟢 ('-' for default)"
+REQUEST_TASK_DUE_DATE = "✍️ Select task's due date.🔴"
+REQUEST_TASK_REPEAT = "✍️ Enter task's repeat time.🟢 ('-' for no repeat)"
+REQUEST_TASK_PRIORITY = "✍️ Select task's priority.🔴"
+
+PRIORITY_BUTTON_1 = "1️⃣"
+PRIORITY_BUTTON_2 = "2️⃣"
+PRIORITY_BUTTON_3 = "3️⃣"
+PRIORITY_BUTTON_4 = "4️⃣"
+
+TASK_REVIEW_TITLE = "😉 Here is your task:\n\n"
+TASK_CONFIRM = "Confirm ❓"
+
+YES_BUTTON = "✅"
+NO_BUTTON = "❌"
+
+
