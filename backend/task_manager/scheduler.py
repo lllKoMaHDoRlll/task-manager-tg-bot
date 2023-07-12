8b168f00acea4f9ac0fe39232ad39d430ff76b69
Koma <aleksandr.orlichenya@gmail.com>
2023-07-12 14:04:22 +0300

    refactor TaskScheduler

--- backend/task_manager/scheduler.py
+++ backend/task_manager/scheduler.py
@@ -1,12 +1,11 @@
-from aiogram import Bot
 from datetime import datetime
 
+from aiogram import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.interval import IntervalTrigger
 from apscheduler.triggers.date import DateTrigger
 
-from backend.task_manager.task_card import TaskCard
 from backend._utils import send_notification
+from backend.task_manager.task_card import TaskCard
 
 
 class TaskScheduler:
@@ -14,17 +13,16 @@ class TaskScheduler:
         self.scheduler = AsyncIOScheduler()
         self.bot = bot
 
-    async def sayhi(self):
-        print("hello")
-
     async def run(self):
-        self.scheduler.add_job(self.sayhi, trigger=IntervalTrigger(seconds=5))
         self.scheduler.start()
 
-
     async def add_task(self, task: TaskCard, chat_id: int):
-        due_date = datetime(year=task.due_date.year, month=task.due_date.month, day=task.due_date.day, hour=13, minute=55)
+        due_date = datetime(
+            year=task.due_date.year,
+            month=task.due_date.month, 
+            day=task.due_date.day,
+            hour=task.due_date.hour,
+            minute=task.due_date.minute
+        )
         trigger = DateTrigger(due_date)
         self.scheduler.add_job(send_notification, trigger=trigger, args=(self.bot, chat_id, task, ))
-        
-
